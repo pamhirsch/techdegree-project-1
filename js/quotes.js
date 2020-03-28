f2cd548d9b41a874fa3daaa7e0b63b4a043f7e14
pamhirsch <pamelahirsch@mac.com>
2020-03-28 11:09:59 -0400

    Create quotes.js

---
+++ js/quotes.js
@@ -0,0 +1,42 @@
+// This file holds the quotes array of quote objects.
+
+var quotes = [
+  {
+    quote: ' Fear is the path to the dark side…fear leads to anger…anger leads to hate…hate leads to suffering.',
+    source: 'Yoda',
+    citation: 'The Phantom Menace',
+    year: '1999',
+    tags: 'movie'
+  },
+  {
+    quote: '“Never, never, never give up.',
+    source: 'Winston Churchill'
+  },
+  {
+    quote: 'You get whatever accomplishment you are willing to declare.',
+    source: "Georgia O'Keefe",
+    tags: 'feminism'
+  },
+  {
+    quote: 'Go confidently in the direction of your dreams. Live the life you have imagined.',
+    source: 'Henry David Thoreau'
+  },
+  {
+    quote: 'Go ahead. Make my day.',
+    source: 'Inspector Harold Francis Callahan',
+    citation: 'Dirty Harry',
+    year: '1971',
+    tags: 'movie'
+  },
+  {
+    quote: 'Every artist was first an amateur',
+    source: 'Ralph Waldo Emerson',
+    tags: 'art'
+  },
+  {
+    quote: 'The world always seems brighter when you’ve just made something that wasn’t there before.',
+    source: 'Neil Gaiman',
+    tags: 'art'
+  },
+
+];
